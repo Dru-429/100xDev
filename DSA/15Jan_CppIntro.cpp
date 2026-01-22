f84fd2f67d63387788e60cbb80afaeff9ac7dea7
Dru-429 <ect.dhruv@gmail.com>
2026-01-22 12:08:11 +0530

    15 jan notes

---
+++ DSA/15Jan_CppIntro.cpp
@@ -0,0 +1,21 @@
+#include <iostream> //Header file which is helping us to use cout
+using namespace std; //This line is used to avoid writing std:: before every standard library object
+
+int main() 
+{
+  int a;
+  int b;
+  int age = 21;
+  cout << "Age:" << endl;
+  cout << age << endl;
+
+  cin >> a;
+  cin >> b;
+  cout << "Sum is: " << a + b << endl;
+
+  return 0;
+}
+
+
+//Data type: int, float, double, char, bool. long long, char = 'a', 
+//Var must be declare 
