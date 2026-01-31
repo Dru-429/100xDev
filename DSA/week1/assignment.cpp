4c7116c77cb08b76dc84ddae034bb91a01f05ce0
Dru-429 <ect.dhruv@gmail.com>
2026-01-31 22:06:13 +0530

    Added Assignment 1 DSA

--- DSA/week1/assignment.cpp
+++ DSA/week1/assignment.cpp
@@ -13,14 +13,67 @@ int main() {
   //   cout << n << "*" << i << "=" << n*i << endl;
   // }
 
-  int len,breadth;
+  // int len,breadth;
 
-  if(cin >> len >> breadth){
-    int area = len*breadth;
-    int parimeter = 2*(len + breadth);
+  // if(cin >> len >> breadth){
+  //   int area = len*breadth;
+  //   int parimeter = 2*(len + breadth);
 
-    cout << "Area = " << area << endl;
-    cout << "Perimeter = " << parimeter << endl;
-  }
+  //   cout << "Area = " << area << endl;
+  //   cout << "Perimeter = " << parimeter << endl;
+  // }
+
+  // int n;
+  // cin >> n;
+
+  // for(int i=1; i <=10; i++) {
+  //   cout << n << " * " << i << " = " << n*i << endl;
+  // }
+
+  // int n, m;
+
+  // if(cin >> n >> m) {
+  //   cout << n << " + " << m << " = " << n+m << endl ;
+  //   cout << n << " - " << m << " = " << n-m << endl ;
+  //   cout << n << " * " << m << " = " << n*m << endl ;
+  //   cout << n << " / " << m << " = " << n/m << endl ;
+  //   cout << n << " % " << m << " = " << n%m << endl ;
+  // }
+
+  // int n,m;
+
+  // cin >> n >> m;
+  // if( n> m ) {
+  //   cout << "Min = " << m << endl;
+  //   cout << "Max = " << n << endl;
+  // }
+  // else{
+  //   cout<< "Min = " << n << endl;
+  //   cout<< "Max = " << m << endl;
+  // }
+
+      int a,b,c ;
+    
+    cin >> a >> b >> c;
+    
+    int max = a;
+    if (b >= max ){
+        max = b;
+    }
+    if (c >= max){
+        max = c;
+    }
+    
+    int min = a;
+    if (b <= min){
+        min = b;
+    }
+    if (c <= min){
+        min = c;
+    }
+    
+    cout << "Min = " <<min << endl;
+    cout << "Max = " <<max << endl;
+    
   return 0;
 }
