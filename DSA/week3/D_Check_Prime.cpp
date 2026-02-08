a704781f0397f0ffdc687a949629ec74fcf7f2dd
Dru-429 <ect.dhruv@gmail.com>
2026-02-08 23:57:37 +0530

    week 3 dsa added

--- DSA/week3/D_Check_Prime.cpp
+++ DSA/week3/D_Check_Prime.cpp
@@ -1,19 +1,33 @@
-#include <iostream> 
+#include <iostream>
 using namespace std;
 
-int main() {
-  int num;
-  bool isPrime = true;
-
-  if(cin >> num){
-    for(int i =2; i <= num/2; i++){
-      if (num%i == 0){
-        isPrime = false;
-      }     
+void checkPrime(long long num)
+{
+    if (num < 2) {
+        cout << "Not Prime" << endl;
+        return;
     }
 
+    bool isPrime = true;
+    // Optimization: Loop up to sqrt(num) using i*i <= num
+    for (long long i = 2; i * i <= num; i++)
+    {
+        if (num % i == 0)
+        {
+            isPrime = false;
+            break; 
+        }
+    }
+    
     cout << (isPrime ? "Prime" : "Not Prime") << endl;
-  }
+}
 
+int main()
+{
+  long long num;
+
+  if (cin >> num){
+    checkPrime(num);
+  }
   return 0;
 }
