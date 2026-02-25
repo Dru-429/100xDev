a15ea1dd8d996f056453f8fbc7cdd5b09df0cf57
Dru-429 <ect.dhruv@gmail.com>
2026-02-25 13:18:58 +0530

    Added week 5 dsa

---
+++ M_Palindrome.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  string str;
+  cin >> str;
+
+  for(int i=0; i<str.length()/2; i++){
+    if(str[i] != str[str.length()-1-i]){
+      cout << "NO";
+      return 0;
+    }
+  }
+  cout << "YES";
+  return 0;
+}
---
+++ N_Count_Words.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  string str;
+  int count= 1;
+  getline(cin, str);
+
+  for (int i=0; i<str.length(); i++){
+    if(str[i] == ' '){
+      count++;
+    }
+  }
+  cout << count << endl;
+  return 0;
+}
