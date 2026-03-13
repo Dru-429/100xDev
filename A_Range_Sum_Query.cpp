e9f0dd1bc6654d542e8471529f8e55e099fbd174
Dru-429 <ect.dhruv@gmail.com>
2026-03-13 23:04:25 +0530

    Added dsa

---
+++ A_Range_Sum_Query.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int len; 
+  cin >> len;
+  long long arr[len];
+  long long prefArr[len];
+  
+  for(int i =0; i < len; i++) {
+    cin >> arr[i];
+  }
+  for(int i =0; i<len; i++){
+    prefArr[i] = arr[i] + (i > 0 ? prefArr[i-1] : 0);
+  }
+
+  int q;
+  cin >> q;
+
+  while (q--)
+  {
+    long long l, r;
+    cin >> l >> r;
+    l--;
+    r--;
+    long long sum = prefArr[r] - (l > 0 ? prefArr[l-1] : 0);
+    cout << sum << endl;
+  }
+
+  return 0;
+}
