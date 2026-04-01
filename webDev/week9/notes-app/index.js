c7a1eef26ec56329d8e7cba627c51dffc99b393b
Dru-429 <ect.dhruv@gmail.com>
2026-04-01 12:48:31 +0530

    Added auth endpoints for notes

--- webDev/week9/notes-app/index.js
+++ webDev/week9/notes-app/index.js
@@ -1,15 +1,78 @@
 const express = require('express');
+const jwt = require('jsonwebtoken')
 
 const app = express();
 
-const notes = [];  //In memory DB for now 
 app.use(express.json());  //Middleware to parse JSON body of requests
+const notes = [];  //In memory DB for now 
+const users = [{
+  username: "dru429",
+  password: "123123"
+}];
+
+app.post('/signup', function(req, res){
+  const username = req.body.username;
+  const password = req.body.password;
+  const userExists = users.find(user => user.username === username);
+  if(userExists){
+    return res.status(403).json({
+      message: "User with this username already exists"
+    })
+  }
+
+  users.push({
+    username, password
+  })
+
+  res.json({
+    message: "User signed up"
+  })
+})
 
 
-//POST for creating a notes
+app.post('/signin', function(req, res){
+  const username = req.body.username;
+  const password = req.body.password;
+  const userExists = users.find(user => user.username === username && user.password === password);
+
+  if(userExists){
+    res.status(403).json({
+      message: "Wrong ID or Password"
+    })
+  }
+
+  //jwt json web tokens
+  const token = jwt.sign({
+    username: username
+  }, "dru429");
+  res.json({
+    token
+  })
+})
+
+//POST for creating a notes - AUTHENTICATION ENDPOINT
 app.post('/notes', (req, res) => {
-  const notes = req.body.note;
-  notes.push(notes);
+
+  //CHECK IF THEY HAD THE RIGHT HEADER and extraxt who is this user 
+  const token = req.headers.token;
+
+  if(!token){
+    res.status(403).send({
+      message: "You r not looged in"
+    })
+  }
+
+  const decoded = jwt.verify(token, "dru429");
+  const username = decoded.username;
+
+  if(!username){
+    res.status(403).send({
+      message: "Malfunction token"
+    })
+  }
+
+  const note = req.body.note;
+  notes.push(note);
 
   res.json({
     message: "done"
@@ -17,16 +80,36 @@ app.post('/notes', (req, res) => {
 });
  
 
-//GET for fetching all notes
+//GET for fetching all notes - AUTHENCATED ENDPOINT
 app.get('/notes', (req, res) =>{
+  const token = req.header.token;
+
+  if(!token) {
+    res.status(403).send({
+      message: "You r not logged in"
+    })
+  }
+   
+  const decode = jwt.verify(token, "dru429");
+  const username = decode.username;
+
+  if(!username){
+    res.status(403).send({
+      meassage: "Malfunction token"
+    })
+  }
+
+  const userNotes = notes.map(note => note.username === username);
   res.json({
-   notes
+   userNotes
   });
 });
 
 //GET for frontend 
-app.get('/', (req, res) => {
-  res.sendFile("C:/Users/RANIYA SAHOO/OneDrive/Deskop/dru.env/100xDev/webDev/week9/notes-app/frotnend/index.html");
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/frontend/index.html');
 })
 
-app.listen(3000)
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+})
